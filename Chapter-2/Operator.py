3955b2c0cf77bcf4ab66ea2c8b0125036fdffdb8
sakshipatel09765-cmyk <sakshipatel09765@gmail.com>
2026-05-19 22:43:49 +0530

    updated files

---
+++ Chapter-2/Operator.py
@@ -0,0 +1,35 @@
+# Arithmatic operator
+
+x=8
+y=2
+
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+print(x%y)
+print(x**y)
+print("\n")
+
+#Comparison operator
+
+print(x==y)
+print(x<y)
+print(x>y)
+print(x<=y)
+print(x>=y)
+print(x!=y)
+print("\n")
+
+# Logical operator
+
+print(x>y and x<y)  # Need both condition to be true for -> true
+print(x>y or x<y)   # If any one condition is true then -> true
+print(not(x>y and x<y))
+print("\n")
+
+# Assignment operator
+x+=y   # x = x + y
+print(x)
+x-=y   # x = x - y
+print(x) 
---
+++ Chapter-2/Practice2.py
@@ -0,0 +1,9 @@
+# Write a program that takes two numbers and prints: their sum, diffrence and product.
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+sum_result = num1 + num2
+difference_result = num1 - num2
+product_result = num1 * num2
+print(f"The sum of {num1} and {num2} is: {sum_result}")
+print(f"The difference of {num1} and {num2} is: {difference_result}")
+print(f"The product of {num1} and {num2} is: {product_result}")
